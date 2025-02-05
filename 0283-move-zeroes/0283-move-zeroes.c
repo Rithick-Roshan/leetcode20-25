8b6db2fd36af7d20fd903675b27efe845a8978b2
RITHICK ROSHAN S <143433981+Rithick-Roshan@users.noreply.github.com>
2025-02-05 21:04:27 +0530

    Time: 0 ms (100%), Space: 19.7 MB (20.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,17 +1,27 @@
+void swap(int * a,int * b){
+    int t= *a;
+    *a=*b;
+    *b=t;
+}
 void moveZeroes(int* nums, int n) {
-    int rs[n];
-    int c=0;
-
+    int i=0;
+    int j=-1;
+     
     for(int i=0;i<n;i++){
-        if(nums[i]!=0){
-            rs[c++]=nums[i];
+        if(nums[i] == 0){
+            j=i;
+            break;
         }
     }
 
-    for(int i=c;i<n;i++){
-        rs[i]=0;
+    if(j== -1) return ;
+
+    for(int i=j+1;i<n;i++){
+           if(nums[i]!=0){
+            swap(&nums[i],&nums[j]);
+            j++;
+           }
     }
-    for(int i=0;i<n;i++){
-        nums[i]=rs[i];
-    }
+
 }
+
